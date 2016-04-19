271b64800c7510b44cccf7d5089a43e0faa0e185
Jacob Bijani <jacob.bijani@gmail.com>
2016-04-19 13:57:39 -0400

    validate username presence, format, case-insensitive uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :username, uniqueness: true
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9]+\Z/ }
 end
