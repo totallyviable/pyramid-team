a49c9238db02eb6f5d386ccc5b23c5c7b5d3ca96
Jacob Bijani <jacob.bijani@gmail.com>
2016-04-19 13:37:20 -0400

    submit login doesn't need a path

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :users
 
   get 'login' => 'user_sessions#new', as: :login
-  post 'login' => 'user_sessions#create', as: :submit_login
+  post 'login' => 'user_sessions#create'
   post 'logout' => 'user_sessions#destroy', as: :logout
 end
