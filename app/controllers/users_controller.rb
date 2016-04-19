19cd93a26286744f2f06ffc84f08f51ab6599391
Jacob Bijani <jacob.bijani@gmail.com>
2016-04-19 13:58:05 -0400

    login user after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        auto_login(@user)
+
         format.html { redirect_to :users, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
