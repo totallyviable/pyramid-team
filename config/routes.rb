ca9fcdb9483c7585753d56c73e40ab6ef3015c06
Jacob Bijani <jacob.bijani@gmail.com>
2016-04-19 12:58:33 -0400

    redo routes with better names

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :user_sessions
   resources :users
 
   get 'login' => 'user_sessions#new', as: :login
+  post 'login' => 'user_sessions#create', as: :submit_login
   post 'logout' => 'user_sessions#destroy', as: :logout
 end
